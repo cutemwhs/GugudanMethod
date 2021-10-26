909c752b0f255fb3ef19b87d23f7dbcb41515219
haeky <haeky@DESKTOP-GQH4R2D>
2021-10-26 19:44:20 +0900

    Gugudan Method

---
+++ src/GugudanMethod.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GugudanMethod {
+	
+	
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for(int i=0; i<result.length; i++) {
+			result[i] = times * (i+1);
+		}
+		
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i= 0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int times = scanner.nextInt();
+		int[] result = calculate(times);
+		print(result);
+	}
+	
+	
+}
